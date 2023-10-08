f02edcaceae4588aada74e8286f0bf4d79d5b4c0
Berdyshev Michael <mberdyshev@list.ru>
2023-10-09 03:22:53 +0500

    Task 1 is solved: Added API for creating robots from json

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,26 @@
+import json
+from http import HTTPMethod, HTTPStatus
+
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 from django.shortcuts import render
 
-# Create your views here.
+from robots.models import Robot
+
+
+def create_robot(request):
+    if request.method != HTTPMethod.POST:
+        return HttpResponseNotAllowed([HTTPMethod.POST])
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError as e:
+        return HttpResponseBadRequest('Invalid json')
+
+    data['serial'] = f'{data.get("model")}-{data.get("version")}'
+    instance = Robot(**data)
+    try:
+        instance.save()
+    except ValidationError as e:
+        return JsonResponse(e.message_dict, status=HTTPStatus.BAD_REQUEST)
+
+    return JsonResponse(instance.id, safe=False, status=HTTPStatus.CREATED)
