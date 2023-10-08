3e03e33c84c8fc82e1cb79f83c78dc52efd0d1fb
Berdyshev Michael <mberdyshev@list.ru>
2023-10-09 02:56:22 +0500

    Added model validators and pre-save signals. Migrations were auto-created.

--- robots/models.py
+++ robots/models.py
@@ -1,8 +1,9 @@
+from django.core import validators
 from django.db import models
 
 
 class Robot(models.Model):
-    serial = models.CharField(max_length=5, blank=False, null=False)
-    model = models.CharField(max_length=2, blank=False, null=False)
-    version = models.CharField(max_length=2, blank=False, null=False)
+    serial = models.CharField(max_length=5, blank=False, null=False, validators=[validators.RegexValidator('^..-..$')])
+    model = models.CharField(max_length=2, blank=False, null=False, validators=[validators.MinLengthValidator(2)])
+    version = models.CharField(max_length=2, blank=False, null=False, validators=[validators.MinLengthValidator(2)])
     created = models.DateTimeField(blank=False, null=False)
